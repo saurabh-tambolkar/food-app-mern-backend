3fc7276b9dfbe39dbe0444758eb1fe1b9cf1a24d
saurabh-tambolkar <saurabhtambolkar22@gmail.com>
2024-01-05 23:57:24 +0530

    3rd

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ connect_mongoDb();
 
 app.use(cors());
 
-const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({ extended: true }));
+// const bodyParser = require("body-parser")
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 
